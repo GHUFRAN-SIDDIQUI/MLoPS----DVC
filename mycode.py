39d19dfe101c768f3ba0043b22af289c7fedb9f6
Ghufran <ghufransiddiqui@zhcet.ac.in>
2025-11-24 14:03:02 +0530

    first commit before CVC

---
+++ mycode.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import os
+data = { 'Name':['Alice', 'Bob', 'Charlie', 'David'],
+         'Age':[24, 27, 22, 32], 
+         'City' : ['New York', 'Los Angeles', 'Chicago', 'Houston']
+         }
+
+df = pd.DataFrame(data)
+
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+#Define file paths
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+#Save the DataFrame to a CSV file, incluidng column name
+df.to_csv(file_path, index = False)
+
+print(f"csv file saved at: {file_path}")
